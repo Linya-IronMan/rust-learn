563b6c6f96c914578d6cd99fc18554db377cbba4
linya <cdthanye32@163.com>
2022-05-15 16:12:16 +0800

    feat(s1 ): client server 二进制 数据的传递

--- s1/tcpserver/src/main.rs
+++ s1/tcpserver/src/main.rs
@@ -1,5 +1,20 @@
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 fn main() {
-    println!("Hello, world!");
+    // 建立监听
+    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
+
+    println!("Running on port 3000 ...");
+
+    // 读取监听到的数据
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+        println!("Connection established!");
+        // 新建长度为 1024 的数组，默认填充 0
+        let mut buffer = [0; 1024];
+
+        stream.read(&mut buffer).unwrap();
+        stream.write(&mut buffer).unwrap();
+    }
 }
