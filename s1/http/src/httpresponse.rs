fb9259e1b6120b943791b06c2c1fef11f009b4f3
linya <cdthanye32@163.com>
2022-05-15 18:44:18 +0800

    feat(s1): http server

---
+++ s1/http/src/httpresponse.rs
@@ -0,0 +1,17 @@
+// derive: 派生
+#[derive(Debug, PartialEq)]
+pub enum Method {
+  Get,
+  Post,
+  Uninitialized,
+}
+
+impl From<&str> for Method {
+  fn from(s: &str) -> Method {
+    match s {
+      "GET" => Method::Get,
+      "POST" => Method::Post,
+      _ => Method::Uninitialized,
+    }
+  }
+}
