5b6e0d5f9ef0b18fbd166709ad877b2134b9a52a
linya <cdthanye32@163.com>
2022-07-20 08:38:52 +0800

    s1页面基础运行

--- 03-tcp_server/s1/httpserver/src/router.rs
+++ 03-tcp_server/s1/httpserver/src/router.rs
@@ -11,14 +11,14 @@ impl Router {
         match req.method {
             httprequest::Method::Get => match &req.resource {
                 httprequest::Resource::Path(s) => {
-                    let route: Vec<&str> = s.split("/").collect();
+                    let route: Vec<&str> = s.split('/').collect();
                     match route[1] {
                         "api" => {
-                            let resp: HttpResponse = WebServiceHandler::handler(&req);
+                            let resp: HttpResponse = WebServiceHandler::handle(&req);
                             let _ = resp.send_response(stream);
                         }
                         _ => {
-                            let resp: HttpResponse = StaticPageHandler::handler(&req);
+                            let resp: HttpResponse = StaticPageHandler::handle(&req);
                             let _ = resp.send_response(stream);
                         }
                     }
